459c52b05f2852d86924d63f6e12f6b58e6d2b2d
Henry Benerman <cyberhank@comcast.net>
2021-02-02 12:03:18 -0700

    problem 1 done

---
+++ Code.py
@@ -0,0 +1,3 @@
+from Rectangle import Rectangle
+newRectangle = Rectangle(12,10)
+print(newRectangle.rectangle_area())
---
+++ Rectangle.py
@@ -0,0 +1,11 @@
+class Rectangle():
+    def __init__(self,width,length):
+        self.width = width
+        self.length = length
+    def rectangle_area(self):
+        area = self.width * self.length
+        return area
+
+
+
+
