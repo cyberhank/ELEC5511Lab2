f9ceaa477791d6d0653a7ffbe892877506bf5cd1
Henry Benerman <cyberhank@comcast.net>
2021-02-03 21:14:31 -0700

    Working on Problem 2

--- Code.py
+++ Code.py
@@ -1,5 +1,6 @@
 from Rectangle import Rectangle
 from Circle import Circle
+from JSON import JSON
 import math
 newRectangle = Rectangle(12,10)
 print(newRectangle.rectangle_area())
@@ -7,3 +8,6 @@ print(newRectangle.rectangle_area())
 NewCircle = Circle(8)
 print(NewCircle.area())
 print(NewCircle.circumference())
+
+newJSON = JSON()
+newJSON.create_json()
