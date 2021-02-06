ce12dbe7c3acbdc94ebba2621408763ad6f63785
Henry Benerman <cyberhank@comcast.net>
2021-02-05 19:28:31 -0700

    Part of problem 3

---
+++ Excel.py
@@ -0,0 +1,15 @@
+import pandas as pd
+class exceler():
+    def __init__(self):
+        return
+    def find_date(self,file):
+        data = pd.read_excel(file, sheet_name='employee')
+        print(data)
+        frame = 0
+        return frame
+
+    pass
+
+
+
+
