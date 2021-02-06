98625e136dc36bb1331b27eb243a0a30d6fb00a3
Henry Benerman <cyberhank@comcast.net>
2021-02-05 21:59:45 -0700

    Problem 3 part 1 done w/ all other code put back

--- Excel.py
+++ Excel.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import re
 class exceler():
     def __init__(self):
         return
     def find_date(self,file):
         data = pd.read_excel(file, sheet_name='Sheet1')
         print(data)
+        print(data[(data.hire_date >= '2007-01-01')])
         frame = 0
         return frame
 
