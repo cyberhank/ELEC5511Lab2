023e060d0a2374d1315bce3fb8ad533a78c941d8
Henry Benerman <cyberhank@comcast.net>
2021-02-07 11:59:34 -0700

    Fixed problem 3.3

--- Code.py
+++ Code.py
@@ -3,16 +3,17 @@ from Rectangle import Rectangle
 from JSONR import jsoncreator
 from Excel import exceler
 
-newCircle = Circle(8)
-print(newCircle.area())
-print(newCircle.circumference())
+#newCircle = Circle(8)
+#print(newCircle.area())
+#print(newCircle.circumference())
 
-newRectangle = Rectangle(12,10)
-print(newRectangle.rectangle_area())
+#newRectangle = Rectangle(12,10)
+#print(newRectangle.rectangle_area())
 
 #newJSON = jsoncreator()
 #newJSON.create_json()
 
 newExcel = exceler()
+#print(newExcel.find_date('employee.xlsx'))
+#print(newExcel.sort_dates('employee.xlsx'))
 print(newExcel.find_date('employee.xlsx'))
-print(newExcel.sort_dates('employee.xlsx'))
--- Excel.py
+++ Excel.py
@@ -12,6 +12,11 @@ class exceler():
         data = pd.read_excel(file, sheet_name='Sheet1')
         print(data)
         print(data.sort_values(by = 'hire_date'))
+    def find_date(self,file):
+        data = pd.read_excel(file, sheet_name='Sheet1')
+        print(data)
+        print(data[((data.hire_date >= '2005-01-01') & (data.hire_date <= '2006-12-31'))])
+        return
     pass
 
 
